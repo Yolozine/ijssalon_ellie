e3b0eb1fb0773abcdb74f0257ea68c646f7eaafb
Jozine van der Knaap <jozinevdknaap@hotmail.com>
2024-12-13 16:57:30 +0100

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,26 +1,27 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5,
-}
-aanbieding = prijzen["aardbei"] * 0.8
-# reclame_tekst = "Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €"
+from helper import decoreer
 
-reclame_tekst = f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €{aanbieding}"
+def print_aanbieding():
+      prijzen = {
+      "aardbei" : 3,
+      "vanille" : 4,
+      "chocolade" : 5,
+      }
+      aanbieding = prijzen["aardbei"] * 0.8
 
-# Deze suggestie kreeg ik van chatGPT, vele malen eenvoudiger te begrijpen dan index en TypeError enzo??
-reclame_tekst2 = f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €{aanbieding:.2f}"
+      reclame_tekst = f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts €{aanbieding}"
 
-# hoofdletters
-reclame_tekst3 = (reclame_tekst2.upper())
+      reclame_tekst2 = reclame_tekst[:63]
 
-# split naar list
-reclame_tekst4 = (reclame_tekst3.split())
+      reclame_tekst3 = reclame_tekst2.upper()
 
-# onder mekaar
-for el in reclame_tekst4:
-  if len(el) >= 5: 
-        print(el.upper())  
-  else:
-        print(el.lower())
+      reclame_tekst4 = reclame_tekst3.split()
 
+      for el in reclame_tekst4:
+          if len(el) > 4: 
+            print(el.upper())  
+      else:
+            print(el.lower())
+
+
+decoreer ("Aanbieding")
+print_aanbieding()
